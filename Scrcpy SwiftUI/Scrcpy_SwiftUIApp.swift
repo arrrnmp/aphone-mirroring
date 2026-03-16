f38abcee7f7833abe6948fda3321b45fbbab271d
Aaron Mompié <github@aaronmompie.com>
2026-03-16 20:52:29 +0100

    Initial Commit

---
+++ Scrcpy SwiftUI/Scrcpy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrcpy_SwiftUIApp.swift
+//  Scrcpy SwiftUI
+//
+//  Created by Aaron Mompié on 16/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Scrcpy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
